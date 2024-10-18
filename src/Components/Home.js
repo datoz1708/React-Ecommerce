807c7021c592755c7256b83192afb135498c2134
datoz1708 <Dato.zarandia.1@gmail.com>
2024-10-19 00:18:39 +0400

    ok

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const firstName = 'დათო'; // სახელი
-  const lastName = 'ზარანდია'; // გვარი
+  const firstName = 'დათო';
+  const lastName = 'ზარანდია';
 
   return (
     <div>
       <h1>Home Page</h1>
       <Link to={`/profile/${firstName}/${lastName}`}>
-        <button>Go to Profile</button>
+        <button>Profile</button>
       </Link>
     </div>
   );
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Profile = () => {
-  const { firstName, lastName } = useParams(); // useParams ჰუკი URL-დან იღებს პარამეტრებს
+  const { firstName, lastName } = useParams();
 
   return (
     <div>
