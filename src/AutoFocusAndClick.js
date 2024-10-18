693926766861301340bce83ff56ccf8a86717ee2
datoz1708 <Dato.zarandia.1@gmail.com>
2024-10-18 23:15:00 +0400

    ok

--- src/AutoFocusAndClick.js
+++ src/AutoFocusAndClick.js
@@ -1,31 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 
 const AutoFocusAndClick = () => {
-  const inputRef = useRef(null); // ფოკუსისთვის
-  const buttonRef = useRef(null); // ღილაკზე ავტომატური დაჭერისთვის
+  const inputRef = useRef(null);
+  const buttonRef = useRef(null);
 
   useEffect(() => {
-    // ავტომატური ფოკუსი input-ზე
     if (inputRef.current) {
       inputRef.current.focus();
     }
 
-    // ავტომატური ღილაკზე დაჭერა გვერდის ჩატვირთვისას
     if (buttonRef.current) {
       buttonRef.current.click();
     }
-  }, []); // ერთხელ შესრულება გვერდის ჩატვირთვისას
+  }, []);
 
   const handleClick = () => {
-    alert("Button was clicked automatically!");
+    alert("OK");
   };
 
   return (
     <div>
-      <input ref={inputRef} placeholder="Focus will be here automatically" />
+      <input ref={inputRef} />
       <br />
       <button ref={buttonRef} onClick={handleClick}>
-        Click Me (automatically clicked)
+        Click Me
       </button>
     </div>
   );
